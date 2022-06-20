d8013c6e43cc7094b4227bbfd49f74933763a97d
Santino Suntaxi <santinosuntaxi@gmail.com>
2022-06-19 22:03:07 -0500

    Update Jenkinsfile.unit.groovy

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -36,10 +36,13 @@ pipeline {
         }
         success {  
              echo "El nombre del trabajo es: ${JOB_NAME}"
-             echo "El numero de ejecucion es: ${EXECUTOR_NUMBER}"
+             echo "El numero de ejecucion es: ${BUILD_NUMBER}"
              
          }  
         
+       failure {  
+            // mail bcc: '', body: "<b>Example</b><br>Project: ${env.JOB_NAME} <br>Build Number: ${env.BUILD_NUMBER} <br> URL de build: ${env.BUILD_URL}", cc: '', charset: 'UTF-8', from: '', mimeType: 'text/html', replyTo: '', subject: "ERROR CI: Project name -> ${env.JOB_NAME}", to: "foo@foomail.com";  
+         }   
         
     }
 }
